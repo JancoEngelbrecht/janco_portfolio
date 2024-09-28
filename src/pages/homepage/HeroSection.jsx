78f498f9dfaf4c17a8c4bf030676e1b2a13eea21
Janco Engelbrecht <engelbrechtjanco@gmail.com>
2024-09-28 10:26:42 +0200

    Add HeroSection to Home pages
    Risk: low

---
+++ src/pages/homepage/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroSection = () => {
+
+    return (
+        <>
+        <div className="">Hello World</div>
+        </>
+    );
+};
+
+export default HeroSection;
