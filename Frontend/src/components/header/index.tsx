b9e3b2db7851b2dd40cb6282a57f821fc085e13c
Janco Engelbrecht <engelbrechtjanco@gmail.com>
2024-11-10 19:36:02 +0100

    Frontend: Add scrollcomponents

--- Frontend/src/components/header/index.tsx
+++ Frontend/src/components/header/index.tsx
@@ -30,8 +30,8 @@ const Header:React.FC = () => {
   }, [menuOpen]);
 
   return (
-    <header className="relative overflow-x-hidden pl-6 w-full h-36 text-white text-6xl font-segoe z-50 bg-gradient-to-br from-neutral-800 from-0% to-neutral-950 to-70%">
-      <div className="flex justify-between items-center w-full">
+    <header className="relative overflow-hidden pl-6 w-full h-36 text-white text-4xl font-segoe z-50 bg-gradient-to-br from-neutral-800 from-0% to-neutral-950 to-70%">
+      <div className="flex justify-between items-center">
         <div className="flex items-center">
           <a href="/" className="hover:animate-bounce"><b>J</b></a>
           <a href="/" className="hover:animate-bounce"><b>.</b></a>
@@ -49,7 +49,7 @@ const Header:React.FC = () => {
             <img
               src={menuicon}
               alt="menu icon"
-              className="w-28 h-auto cursor-pointer pt-4 pr-6 "
+              className="w-16 h-auto cursor-pointer pt-4 pr-6 "
             />
           </div>
 
@@ -58,11 +58,14 @@ const Header:React.FC = () => {
             id="menu"
             className={`absolute top-0 right-0 flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-8 p-4 rounded-lg transition-transform ${menuOpen ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"} duration-500 ease-in-out z-40`} 
           >
-            <a href="/contact" className="text-white text-2xl cursor-pointer">
+            <a href="/contact" className="text-white text-xl cursor-pointer">
               Contact
             </a>
-            <a href="/projects" className="text-white text-2xl cursor-pointer">
+            <a href="/projects" className="text-white text-xl cursor-pointer">
               Projects
+            </a>
+            <a href="/playground" className="text-white text-xl cursor-pointer">
+              Playground
             </a>
           </div>
         </div>
--- Frontend/src/pages/playgroundpage/navcomponents/index.tsx
+++ Frontend/src/pages/playgroundpage/navcomponents/index.tsx
@@ -3,26 +3,35 @@ import React from 'react';
 import Dropdown from './dropdown';
 import Sidebar from './sidebar';
 import Searchbar from './searchbar';
-import PlaneImage from '../../../assets/plane.jpg'
+import PlaneImage from '../../../assets/plane.jpg';
 
 const Index: React.FC = () => {
   return (
-    <div className="relative flex justify-center items-center w-screen h-screen">
-      <div className="relative flex flex-col h-4/5 w-2/3 bg-slate-100 border border-gray-300 shadow-md rounded-xl overflow-hidden">
+    <>
+      {/* Navigation Header */}
+      <header className="w-full text-center py-4 text-gray text-lg sm:text-xl font-semibold">
+        Navigation
+      </header>
 
-      {/* Background image with responsive styling */}
-      <img src={PlaneImage} alt='Plane window' className='absolute inset-0 w-full h-full object-cover z-0'></img>
-     
+      {/* Main Component */}
+      <div className="relative flex justify-center h-screen">
+        <div className="relative flex flex-col w-full h-full sm:w-3/4 sm:h-4/5 bg-slate-100 border border-gray-300 shadow-md rounded-xl overflow-hidden">
 
-        <div className='flex w-full z-20'>
-          <Dropdown />
-        </div>
-        <div className='flex h-full'>
-          <Sidebar />
-          <div className='flex w-full justify-center items-center z-10'><Searchbar /></div>
+          {/* Background image with responsive styling */}
+          <img src={PlaneImage} alt="Plane window" className="absolute inset-0 w-full h-full object-cover z-0" />
+
+          <div className="flex w-full z-20">
+            <Dropdown />
+          </div>
+          <div className="flex h-full">
+            <Sidebar />
+            <div className="flex w-full justify-center items-center z-10">
+              <Searchbar />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
